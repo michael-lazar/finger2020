4634f09f9f0ce9d2ae5d702e93dc27526ad5f83d
Michael Lazar <mlazar@doctorondemand.com>
2020-04-27 23:23:53 -0400

    Update docs and install script

--- install.sh
+++ install.sh
@@ -3,9 +3,15 @@
 echo "Installing /usr/bin/finger2020"
 install finger2020 /usr/bin/finger2020
 
-echo "Setting up systemd service..."
+echo "Installing systemd files..."
 install systemd/finger2020.socket /etc/systemd/system/finger2020.socket
 install systemd/finger2020@.service /etc/systemd/system/finger2020@.service
 install -b systemd/finger2020.env /etc/sysconfig/finger2020.env
+
+echo "Enabling systemd service..."
+systemctl daemon-reload
+systemctl enable finger2020.socket
+systemctl start finger2020.socket
+systemctl status finger2020.socket
 
 echo "Done!"
